a7648cf5e994ae5dd4734fd43f580463a4386774
zouhang <zouhang@didiglobal.com>
2023-04-17 12:44:37 +0800

    chore: update icons

--- src/components/ui/context-menu.tsx
+++ src/components/ui/context-menu.tsx
@@ -123,7 +123,7 @@ const ContextMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuPrimitive.ItemIndicator>
-        <span className="i-lucide:circle flex h-2 w-2 text-current" />
+        <span className="i-tabler:circle-filled flex h-2 w-2 text-current" />
       </ContextMenuPrimitive.ItemIndicator>
     </span>
     {children}
