a32cbde56f97d1b24d114f096ea8100689a61fd9
zouhang <1102639563@qq.com>
2023-04-26 22:40:40 +0800

    chore: update

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { VariantProps, cva } from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background transition-transform active:scale-97",
