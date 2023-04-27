f162139e71a8d73bbd4ecfc4b0a44af12ff8bfb1
zouhang <zouhang@didiglobal.com>
2023-04-27 14:13:49 +0800

    chore: update path

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastViewport } from '@/components/ui/toast'
-import { useToast } from '@/components/ui/use-toast'
+import { useToast } from './use-toast'
 
 export function Toaster() {
   const { toasts } = useToast()
