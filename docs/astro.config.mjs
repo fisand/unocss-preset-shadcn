f97862ce1ca0bb39b6e212ad41b4179083342c23
zouhang <zouhang@didiglobal.com>
2023-05-04 16:27:49 +0800

    chore: update layout

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
   root: './',
-  integrations: [mdx(), react()],
+  integrations: [
+    mdx(),
+    react(),
+    UnoCSS(),
+  ],
 })
