e2c6416f25b0dbb5c03e0e7be42c7fe80fa3e564
zouhang <zouhang@didiglobal.com>
2023-05-04 11:50:04 +0800

    chore: update docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+
+// https://astro.build/config
+export default defineConfig({
+  root: './',
+  integrations: [mdx(), react()],
+})
