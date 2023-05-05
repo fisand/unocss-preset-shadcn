333557749dd5ef7b06fda986d2cfcf79bccc83f2
zouhang <zouhang@didiglobal.com>
2023-05-05 19:03:10 +0800

    chore: update

---
+++ docs/App.tsx
@@ -0,0 +1,9 @@
+import Header from './components/Header.mdx'
+import SideBar from './components/SideBar.mdx'
+
+export default function App() {
+  return <div className='min-h-screen'>
+    <Header />
+    <SideBar />
+  </div>
+}
--- docs/main.tsx
+++ docs/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './index.mdx'
+import App from './App'
 
 import '@unocss/reset/antfu.css'
 import 'uno.css'
