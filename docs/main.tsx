9537945d465bcb496ea47ff3ae6f8aefca17dc0d
zouhang <zouhang@didiglobal.com>
2023-05-06 13:46:35 +0800

    chore: update demo

--- docs/main.tsx
+++ docs/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 
 import './index.css'
+// import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
