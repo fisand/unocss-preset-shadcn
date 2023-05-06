fc302f885138de3742e22e460d6fd3e91c1e971a
zouhang <zouhang@didiglobal.com>
2023-05-06 16:21:20 +0800

    chore: update copy handler

--- docs/App.tsx
+++ docs/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useRoutes } from 'react-router-dom'
 
 import routes from '~react-pages'
@@ -5,13 +6,56 @@ import routes from '~react-pages'
 import Header from './layouts/Header.mdx'
 import SideBar from './layouts/SideBar.mdx'
 
+const extraClassList = [
+  'relative absolute right-4 inline-flex w-6 h-6 bg-gray-500 cursor-pointer i-tabler:checks i-tabler:copy',
+]
+
 export default function App() {
+  useEffect(() => {
+    let timerId: number | undefined = undefined
+
+    async function copyHandler(e: MouseEvent) {
+      if (
+        e.target &&
+        (e.target as Element).tagName.toLowerCase() === 'span' &&
+        (e.target as Element).classList.contains('shadcn-copy')
+      ) {
+        const target = e.target as HTMLSpanElement
+        const toggleClass = () => {
+          target.classList.toggle('i-tabler:copy')
+          target.classList.toggle('i-tabler:checks')
+        }
+
+        if (timerId) {
+          window.clearTimeout(timerId)
+          timerId = undefined
+        }
+
+        if (target.dataset.code) {
+          await navigator.clipboard.writeText(target.dataset.code)
+          toggleClass()
+
+          timerId = window.setTimeout(() => {
+            toggleClass()
+          }, 2000)
+        }
+      } else {
+        return false
+      }
+    }
+    if (window) {
+      window.addEventListener('click', copyHandler)
+    }
+
+    return () => window.removeEventListener('click', copyHandler)
+  }, [])
+
   return (
     <div className="min-h-screen">
       <Header />
       <main className="flex">
         <SideBar />
-        <div className='markdown-body overflow-auto flex-1 py-4 px-8'>{useRoutes(routes)}</div>
+        <div className="markdown-body overflow-auto flex-1 py-4 px-8">{useRoutes(routes)}</div>
       </main>
     </div>
   )
--- vite.doc.config.ts
+++ vite.doc.config.ts
@@ -5,6 +5,8 @@ import Unocss from 'unocss/vite'
 import mdx from '@mdx-js/rollup'
 import Pages from 'vite-plugin-pages'
 import rehypeHighlight from 'rehype-highlight'
+import { visit } from 'unist-util-visit'
+import { toString } from 'hast-util-to-string'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +20,37 @@ export default defineConfig({
     react(),
     Unocss(),
     mdx({
-      rehypePlugins: [rehypeHighlight]
+      rehypePlugins: [
+        rehypeHighlight,
+        () => {
+          return (tree) => {
+            visit(tree, 'element', (node) => {
+              if (['pre'].includes(node.tagName)) {
+                const code = node.children.find((child) => child.tagName === 'code')
+                const stringContent = toString(code)
+
+                const genCopyBtn = () => {
+                  function handleCopy(e) {
+                    console.log(e)
+                  }
+
+                  return {
+                    type: 'element',
+                    tagName: 'span',
+                    properties: {
+                      className: ['shadcn-copy absolute right-4 inline-flex bg-gray-500 w-6 h-6 cursor-pointer i-tabler:copy'],
+                      dataCode: stringContent,
+                    }
+                  }
+                }
+
+                node.properties.className = [...(node.properties.className ?? []), 'relative']
+                node.children.unshift(genCopyBtn())
+              }
+            })
+          }
+        },
+      ],
     }),
     Pages({
       dirs: 'pages',
