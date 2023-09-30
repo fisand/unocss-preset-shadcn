f79cda93e2829c52f10909ff244306f6597dbd0e
zouhang <1102639563@qq.com>
2023-09-30 09:46:04 +0800

    chore: remove useless comps

--- vue-shadcn/ui/context-menu/ContextMenuShortcut.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { cn } from '@/lib/utils'
-</script>
-
-<template>
-  <span :class="cn('ml-auto text-xs tracking-widest text-muted-foreground', $attrs.class ?? '')">
-    <slot />
-  </span>
-</template>
